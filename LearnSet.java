3105896a3893415e9dde31b41148b0cad61b8049
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2024-06-27 11:33:43 +0530

    Linked HashSet

--- LearnSet.java
+++ LearnSet.java
@@ -1,9 +1,11 @@
-import java.util.HashSet;
+// import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class LearnSet {
     public static void main(String[] args) {
-        Set<Integer> set = new HashSet<>();
+        // Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new LinkedHashSet<>();
         set.add(2);
         set.add(32);
         set.add(22);
