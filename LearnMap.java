1b3095a3311f5c1552fd787be08d5f905fc6f746
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2024-06-27 12:37:29 +0530

    Hash-Map

---
+++ LearnMap.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LearnMap {
+    public static void main(String[] args) {
+        Map<String, Integer> numbers = new HashMap<>();
+        numbers.put("one", 1);
+        numbers.put("two", 2);
+        numbers.put("three", 3);
+        numbers.put("four", 4);
+
+        // This will override the above key with 'two'.
+        // numbers.put("two", 22);
+
+        // System.out.println(numbers.containsKey("two"));
+
+        // To check whether the key is present or not.
+        if (!numbers.containsKey("two")) {
+            numbers.put("two", 22);
+        }
+
+        // To check whether the value is present or not.
+        if (numbers.containsValue(2)) {
+            System.out.println(numbers.containsValue(2));
+        }
+
+        // Similar to above if-condition
+        numbers.putIfAbsent("two", 2);
+        System.out.println("Numbers is : " + numbers);
+
+        // Iterating over the hash map
+        for (Map.Entry<String, Integer> e : numbers.entrySet()) {
+            System.out.println(e);
+
+            System.out.println("Key is : " + e.getKey() + " and its value is : " + e.getValue());
+        }
+
+        // To iterate over teh key-set
+
+        for (String key : numbers.keySet()) {
+            System.out.println(key);
+        }
+
+        // To iterate over the values of hash-map
+
+        for (Integer value : numbers.values()) {
+            System.out.println(value);
+        }
+    }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,3 @@
-set.add(24);
+if (!numbers.containsKey(2)) {
+            numbers.put("two", 22);
+        }
